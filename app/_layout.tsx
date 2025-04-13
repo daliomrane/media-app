e2c7c925d9170153f3aff3dc84c03259c3819326
dali <dalibouomrane@gmail.com>
2025-04-13 17:44:16 -0400

    update-image-header-bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,18 +8,26 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Stack>
+    <Stack screenOptions={{ 
+      headerShown: false // This will hide the header bar completely
+    }}>
       <Stack.Screen 
         name="index" 
         options={{ 
-          title: 'Cozey Leaf',
+          //title: 'Cozey Leaf',
           headerShown: false,
-          headerStyle: {
-            backgroundColor: colorScheme === 'dark' ? '#1D3D47' : '#A1CEDC',
-          },
-          headerTintColor: colorScheme === 'dark' ? '#fff' : '#000',
+          //headerStyle: {
+          //  backgroundColor: colorScheme === 'dark' ? '#1D3D47' : '#A1CEDC',
+          //},
+          //headerTintColor: colorScheme === 'dark' ? '#fff' : '#000',
         }} 
       />
+      <Stack screenOptions={{ 
+      headerShown: false // This will hide the header bar completely
+    }}>
+      <Stack.Screen name="[id]" />
+      </Stack>
+
     </Stack>
   );
 }
